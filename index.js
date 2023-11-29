0c68de73e6c130ac29e58cac06de08713ad3a6f4
Ahammad204 <Ahammadcode@gmail.com>
2023-11-29 19:41:48 +0600

    Get and Post pet

--- index.js
+++ index.js
@@ -54,8 +54,42 @@ async function run() {
 
         const categoryCollection = client.db('categoryDB').collection('category')
         const usersCollection = client.db('usersDB').collection('users')
+        const petCollection = client.db('petsDB').collection('pets')
 
+        //Own MiddleWare
+        //Verify Token
 
+        const verifyToken = (req, res, next) => {
+
+            if (!req.headers.authorization) {
+
+                return res.status(401).send({ message: 'Forbidden Access' })
+
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+
+                if (err) {
+
+                    return res.status(401).send({ message: 'Forbidden Access' })
+
+                }
+                req.decoded = decoded;
+                next()
+
+            })
+        }
+        // use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
         //JWT Related api
         app.post('/jwt', async (req, res) => {
 
@@ -72,7 +106,7 @@ async function run() {
 
 
         //Post A Category
-        app.post('/category', async (req, res) => {
+        app.post('/category', verifyAdmin, verifyToken, async (req, res) => {
 
             const newCategory = req.body;
             const result = await categoryCollection.insertOne(newCategory)
@@ -95,15 +129,47 @@ async function run() {
             const query = { email: user.email }
             const existingUser = await usersCollection.findOne(query)
             if (existingUser) {
-      
-              return res.send({ message: 'User already Exists', insertedId: null })
-      
+
+                return res.send({ message: 'User already Exists', insertedId: null })
+
             }
-      
+
             const result = await usersCollection.insertOne(user);
             res.send(result)
-      
-          })
+
+        })
+
+        //Get admin
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        })
+        //Post a Pet
+        app.post('/pet', verifyToken, async (req, res) => {
+
+            const item = req.body;
+            const result = await petCollection.insertOne(item);
+            res.send(result)
+
+        })
+        //Get Pet Data
+        app.get('/pet', async (req, res) => {
+
+            const result = await petCollection.find().toArray();
+            res.send(result)
+
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
